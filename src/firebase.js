bd227a632d3fecdb10c9b18b1affb65eb39408da
younganJung1115 <youngan.jung@gmail.com>
2020-09-27 05:52:11 +0000

    add firestore function

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCbQaV-FxAjXK3m8SE5LJBhn9IoOwEqaVc",
+    authDomain: "reservsportsfacility.firebaseapp.com",
+    databaseURL: "https://reservsportsfacility.firebaseio.com",
+    projectId: "reservsportsfacility",
+                    storageBucket: "reservsportsfacility.appspot.com",
+    messagingSenderId: "337057661600",
+    appId: "1:337057661600:web:d46ac1ab76f157c58f3fc5",
+    measurementId: "G-2HR2HL64D7"
+  };
+
+  firebase.initializeApp(firebaseConfig);
--- src/reservation.js
+++ src/reservation.js
@@ -1,11 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Reservation = () => {
-	return(
+class Reservation extends Component {
+	state = {
+		reservations: [
+			{ todo: '할일1' },
+			{ todo: '할일2' },
+			{ todo: '할일3' },
+		],
+		reservation: ''
+	}
+
+	onClickHandler = (e) => {
+		e.preventDefault();
+		const reservation = { todo: this.state.reservation };
+		const reservations = [...this.state.reservations, reservation]
+		this.setState({
+			reservations,
+			reservation: ''
+		})
+	}
+	onChangeHandler = (e) => {
+		this.setState({
+			reservation: e.target.value
+		})
+	}
+	deleteHandler = (index) => {
+		const reservations = this.state.reservations.filter((reservation, i) => i !== index)
+		this.setState({ reservations });
+	}
+
+	render() {
+		return (
+			<div>
+				<ReservationAdd
+					value={this.state.reservation}
+					changeHandler={this.onChangeHandler}
+					clickHandler={this.onClickHandler}
+				/>
+				<ReservationDisplay
+					reservations={this.state.reservations}
+					deleteHandler={this.deleteHandler} />
+			</div >
+		)
+	}
+}
+
+const ReservationAdd = ({ value, changeHandler, clickHandler }) => {
+	return (
 		<div>
-			<h2>Make a reservation~</h2>
+			<form>
+				<input value={value} onChange={changeHandler}></input>
+				<button onClick={clickHandler}>저장</button>
+			</form>
 		</div>
 	)
+}
+
+const ReservationDisplay = ({ reservations, deleteHandler }) => {
+	const reservationDisplay = reservations.map((reservation, index) => {
+		return (
+			<div key={index}>
+				<p>{reservation.todo}</p>
+				<button onClick={() => deleteHandler(index)}>삭제</button>
+			</div>
+		)
+	})
+	return reservationDisplay
 }
 
 export default Reservation
